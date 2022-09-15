4f87fbca4cd42617962372de6e2337d665d488d5
Gursehaj Singh Arora <gursehaj@bigthinx.com>
2022-09-16 02:24:23 +0530

    did something

--- src/arka.py
+++ src/arka.py
@@ -7,21 +7,29 @@ from kivy.clock import Clock
 from imutils.video import FPS
 import cv2
 
+videoFR = 30.0
+
 class DemoCapture(MDApp):
     def build(self):
         layout = MDBoxLayout(orientation= 'vertical')
         self.image = Image()
         layout.add_widget(self.image)
         self.capture = cv2.VideoCapture('../backgroundVideos/2.mp4')
-        Clock.schedule_interval(self.load_frame, 1.0/1.0)
+        print("Video original FPS: " + str(self.capture.get(cv2.CAP_PROP_FPS)))
+        videoFR = self.capture.get(cv2.CAP_PROP_FPS)
+        Clock.schedule_interval(self.load_frame, 1.0/videoFR)
         return layout
     def load_frame(self, *args):
         ret, frame = self.capture.read()
+        if not ret:
+            self.capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            print('repeating')
+            ret, frame = self.capture.read()
         self.image_frame = frame
-        buffer = cv2.flip(frame,0).tostring()
+        buffer = cv2.flip(frame,0).tobytes()
         texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
         self.image.texture = texture
 
-if __name__ == '__main__':
-    DemoCapture().run()
+    def changeBackground(self):
+        self.capture = cv2.VideoCapture('../backgroundVideos/1.mp4')
